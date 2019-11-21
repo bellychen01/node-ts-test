16604f2e05a88649686da0ff8ec7cd2421639a12
BellyChen <chly5281750@gmail.com>
2019-11-21 15:25:03 +0800

    搭建express服务

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
 console.log(`test typescript. ${Date.now()}`)
+import {add} from './utils'
+
+console.log(add(1,2))
+
+import express, {Express, Request, Response} from 'express'
+
+const app:Express = express()
+
+app.use(express.json())
+app.use(express.urlencoded())
+
+app.get('/',(req:Request, res:Response) => {
+  console.log(req.method, req.query, req.body, req.params, req.url)
+  res.json({a: 1, b: 2})
+})
+
+app.post('/', (req:Request, res:Response) => {
+  console.log(req.method, req.query, req.body, req.params, req.url)
+  res.json(req.body)
+})
+
+const port: any = process.env.PORT || 8888
+
+const main = () => {
+  app.listen(port, () => {
+    console.log(`Running on http://localhost:${port}`)
+  })
+}
+
+main()
